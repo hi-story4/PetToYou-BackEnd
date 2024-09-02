dece038a9ab7a185b23d0a1bcefc91cde172862c
박성춘 <songchoon10@naver.com>
2024-09-02 20:22:40 +0900

    #78 feat: MongoDB 적용

--- src/main/java/com/pettoyou/server/constant/dto/ApiResponse.java
+++ src/main/java/com/pettoyou/server/constant/dto/ApiResponse.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;

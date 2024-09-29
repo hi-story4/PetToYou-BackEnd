5d956100932fd512de11e32d9c7ce42139294c18
박성춘 <songchoon10@naver.com>
2024-09-29 19:39:16 +0900

    chore: 페이징시 기본으로 8개의 데이터를 제공해주는것으로 변경

--- src/main/java/com/pettoyou/server/domains/hospital/controller/HospitalController.java
+++ src/main/java/com/pettoyou/server/domains/hospital/controller/HospitalController.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,6 +32,7 @@ public class HospitalController {
     @GetMapping()
     public ResponseEntity<ApiResponse<Page<HospitalDtoWithDistance>>> getHospitalList(
 //            @PageableDefault(size = 20, sort = "created_at", direction = Sort.Direction.DESC)
+            @PageableDefault(size = 8)
             Pageable pageable,
             @ModelAttribute HospitalQueryAddressInfo queryInfo,
             @ModelAttribute HospitalQueryCond queryCond
